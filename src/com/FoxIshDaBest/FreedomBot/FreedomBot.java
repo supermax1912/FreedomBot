26a87421b31afb2a413e022a1a61cb8b4b005c45
FoxIshDaBest <foxishdabest@gmail.com>
2016-02-14 21:29:58 -0800

    Major Bugfixes

--- src/com/FoxIshDaBest/FreedomBot/FreedomBot.java
+++ src/com/FoxIshDaBest/FreedomBot/FreedomBot.java
@@ -1,8 +1,15 @@
 package com.FoxIshDaBest.FreedomBot;
 
+// FreedomBot Imports
+import com.FoxIshDaBest.FreedomBot.Commands.Command_adminhelp;
 import com.FoxIshDaBest.FreedomBot.Commands.Command_freedombot;
+import com.FoxIshDaBest.FreedomBot.Commands.Command_opme;
 import com.FoxIshDaBest.FreedomBot.Listener.BotListener;
+
+// Java Imports
 import java.util.logging.Level;
+
+// Bukkit Imports
 import org.bukkit.Server;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.PluginManager;
@@ -29,6 +36,9 @@ public class FreedomBot extends JavaPlugin {
         getConfig().options().copyDefaults(true);
         saveConfig();
     this.getCommand("freedombot").setExecutor(new Command_freedombot(plugin));
+    this.getCommand("opme").setExecutor(new Command_opme(plugin));
+    this.getCommand("adminhelp").setExecutor(new Command_adminhelp(plugin));
+    
     }
 
     @Override
