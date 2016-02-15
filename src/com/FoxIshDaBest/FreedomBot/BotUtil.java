e1fc279bfbd757108085b18f463f49d28cce46f5
FoxIshDaBest <foxishdabest@gmail.com>
2016-02-14 21:13:54 -0800

    Bugfixes

--- src/com/FoxIshDaBest/FreedomBot/BotUtil.java
+++ src/com/FoxIshDaBest/FreedomBot/BotUtil.java
@@ -6,5 +6,4 @@ public class BotUtil {
     public static final String YOU_ARE_OP = ChatColor.YELLOW + "You are now op!";
     public static final String YOU_ARE_NOT_OP = ChatColor.YELLOW + "You are no longer op!";
     
-    // TODO: Add more stuffs
 }
